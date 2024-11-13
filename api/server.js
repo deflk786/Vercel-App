2fa4ccdad626a2f34e1806189ddd09c1b4f46da5
deflk786 <secdefmodlk@gmail.com>
2024-11-13 16:18:34 +0530

    new commit

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Vercel!');
+  res.send('Vercel Server Working');
 });
 
 module.exports = app;
